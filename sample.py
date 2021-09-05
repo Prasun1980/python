6b86ddeebb8f044f9ff7e234dcbadf1fd9d8ca25
Prasun1980 <61043395+Prasun1980@users.noreply.github.com>
2021-09-05 20:31:56 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
-import sqlite3
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
 
-conn = sqlite3.connect('test.db')
-
-print ("Opened database successfully");
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
 
 
